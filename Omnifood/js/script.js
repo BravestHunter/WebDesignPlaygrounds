5f5aa61648192fe6d0fe2144f9bb6f9d976bd5ed
Vitaly <vasilvvmain@gmail.com>
2023-04-30 16:39:02 +0200

    Added added sticky navbar on  page in Omnifood project

--- Omnifood/js/script.js
+++ Omnifood/js/script.js
@@ -1,6 +1,6 @@
 // Make mobile navigation work
-const navigationButtonElement = document.querySelector(".btn-mobile-nav");
 const headerElement = document.querySelector(".header");
+const navigationButtonElement = document.querySelector(".btn-mobile-nav");
 
 navigationButtonElement.addEventListener("click", function () {
   headerElement.classList.toggle("nav-open");
@@ -10,6 +10,27 @@ navigationButtonElement.addEventListener("click", function () {
 const copyrightYearElement = document.querySelector(".copyright-year");
 const currentYear = new Date().getFullYear();
 copyrightYearElement.textContent = currentYear;
+
+// Sticky navigation
+const heroSectionElement = document.querySelector(".section-hero");
+
+const observer = new IntersectionObserver(
+  function (entries) {
+    const entry = entries[0];
+    if (!entry.isIntersecting) {
+      document.body.classList.add("sticky");
+    } else {
+      document.body.classList.remove("sticky");
+    }
+  },
+  {
+    /* In the viewport */
+    root: null,
+    rootMargin: "-80px",
+    threshold: 0,
+  }
+);
+observer.observe(heroSectionElement);
 
 ///////////////////////////////////////////////////////////
 // Fixing flexbox gap property missing in some Safari versions
